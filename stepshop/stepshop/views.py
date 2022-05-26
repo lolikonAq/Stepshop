59f8d850f207b2b88961bc3ec45776559acb0c21
ITstart <itstart@itstep.loc>
2022-05-26 16:59:21 +0600

    add: lesson_02 files

---
+++ stepshop/stepshop/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contacts(request):
+    return render(request, 'contacts.html')
