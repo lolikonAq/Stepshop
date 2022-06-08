4cc99a3d3f28f2ce41f93855f0ea1c23b8ee769e
ITstart <itstart@itstep.loc>
2022-06-08 17:42:32 +0600

    lesson_07

--- stepshop/authapp/forms.py
+++ stepshop/authapp/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django import forms
 
 from authapp.models import ShopUser
 
@@ -11,5 +12,36 @@ class ShopUserLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(ShopUserLoginForm, self).__init__(*args, **kwargs)
 
-        for fiel_name, field in self.fields.items():
+        for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+
+
+class ShopUserRegisterForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=128,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Логин',
+            }
+        )
+    )
+
+    class Meta:
+        model = ShopUser
+        fields = {'username', 'first_name', 'password1', 'password2', 'email', 'age', 'avatar'}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field_help_text = ''
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+
+        if data < 16:
+            raise forms.ValidationError('Вы слишком молоды!')
+
+        return data
